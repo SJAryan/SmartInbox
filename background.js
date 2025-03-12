120ec278ad21111a3c1e4cadc5efa54a18a21b48
Aryan <Aryan.sanojraj@gmail.com>
2025-03-12 07:37:21 -0500

    working

---
+++ background.js
@@ -0,0 +1,10 @@
+fetch("http://<34.122.217.188>:5000/chat", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ message: "Hello from Chrome Extension" })
+})
+.then(response => response.json())
+.then(data => console.log(data.response))
+.catch(error => console.error("Error:", error));
