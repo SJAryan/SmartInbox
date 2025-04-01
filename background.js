e606c33e35306a7b6c34729e8668e7f563282764
Aryan <Aryan.sanojraj@gmail.com>
2025-04-01 15:34:32 -0500

    FixManifest

--- background.js
+++ background.js
@@ -52,19 +52,19 @@ function getAuthTokenAndFetchEmails() {
   }
   
   // Trigger the process (e.g., when the extension starts, or user clicks a button) 
-  window.onload = getAuthTokenAndFetchEmails;
+  window.onload = getAuthTokenAndFetchEmails();
 
   
 
-var message = 
+// var message = 
 
-fetch("http://34.122.217.188:5000/chat", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ message: "Hello from Chrome Extension" })
-})
-.then(response => response.json())
-.then(data => console.log(data.response))
-.catch(error => console.error("Error:", error));
+// fetch("http://34.122.217.188:5000/chat", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({ message: "Hello from Chrome Extension" })
+// })
+// .then(response => response.json())
+// .then(data => console.log(data.response))
+// .catch(error => console.error("Error:", error));
