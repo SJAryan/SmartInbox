ac69fdf1a6252dd4c796c89f2be9bb460b074b58
Aryan <Aryan.sanojraj@gmail.com>
2025-05-12 18:46:45 -0500

    FixAPI

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from importlib.metadata import requires
 from multiprocessing import process
+import os
 from openai import OpenAI 
 
 import json
-requires('dotenv').config()
+
 
 
 
 
 client = OpenAI(
-  api_key= process.env.OPENAI_API_KEY
-)
+  api_key= "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"
+) 
 prompt = "Act as an assistant who helps individuals by providing them with concise and informative summaries of their emails. You need to make sure that the exact tone and idea is correctly passed on the the user, so don’t make assumptions; always stick to the facts.  Being short and concise in summaries is very important because your job is to help users spend less time checking their emails. Also, there is no need transtion words just just the summaires one after another with asimecolon in between.   You will receive the most recent emails in this format: “ first message: Subject:  Body of Message: Next Message: Subject:  Body of Message: …”.  The first message represents the most recent email in the user's inbox; there can be up to 5 total messages in one message. The summaries need to get straight to the point and include all the information in the email. For example, An email asking the user about how they are doing, what is going on in their life, and other information about how they have been going places and asking the user if they can have a meeting to talk about some things. The output should be: “the sender,” you can try to find the name of the sender from the closing or signature or just have the sender, but the name is preferred, wants to have a meeting to discuss things; also the sender is doing the good, traveling, and inquires about your wellbeing. To further explain the example of the output you need to produce, the most important/fastest action required information should come before a less important one. Also, if there is more than one email make to put most important/fastest action need summaires first. Also the add this symbol \ between each summary. Focus only on the core action or information, omitting pleasantries. Maximum 1 sentence per summary.  Input:"
 
 
